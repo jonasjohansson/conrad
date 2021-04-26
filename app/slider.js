e97fa359f74f9a44d4030d2cafb71146316e813c
Jonas Johansson <jns.johansson@gmail.com>
2021-04-26 23:25:55 +0200

    update

--- app/slider.js
+++
@@ -1,8 +0,0 @@
-import { html } from 'https://unpkg.com/lit-html/lit-html.js';
-import { component } from 'https://unpkg.com/haunted/haunted.js';
-
-function Slider() {
-	return html` <div class="module">Hello, World!</div> `;
-}
-
-customElements.define('x-slider', component(Slider));
